ba8510f870a3e6d4572169d5897a6ff7d492bd8a
marzato <marzato32@gmail.com>
2020-12-14 18:35:04 +0000

    ready

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+import prota from "../../img/prota.png";
+import Yuzu from "../../img/Yuzu.png";
+import Atsuro from "../../img/Atsuro.png";
+import Midori from "../../img/Midori.png";
 const Card = () => {
 	return (
 		<div className="row row-cols-1 row-cols-md-3 g-4">
 			<div className="col">
 				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
+					<img src={prota} className="card-img-top" alt="..." />
 					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
+						<h5 className="card-title">Prota</h5>
 						<p className="card-text">
 							This is a longer card with supporting text below as
 							a natural lead-in to additional content. This
@@ -18,18 +21,18 @@ const Card = () => {
 			</div>
 			<div className="col">
 				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
+					<img src={Yuzu} className="card-img-top" alt="..." />
 					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
+						<h5 className="card-title">Yuzu</h5>
 						<p className="card-text">This is a short card.</p>
 					</div>
 				</div>
 			</div>
 			<div className="col">
 				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
+					<img src={Atsuro} className="card-img-top" alt="..." />
 					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
+						<h5 className="card-title">Atsuro</h5>
 						<p className="card-text">
 							This is a longer card with supporting text below as
 							a natural lead-in to additional content.
@@ -39,9 +42,9 @@ const Card = () => {
 			</div>
 			<div className="col">
 				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
+					<img src={Midori} className="card-img-top" alt="..." />
 					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
+						<h5 className="card-title">Midori</h5>
 						<p className="card-text">
 							{
 								"This is a longer card with supporting text below as a natural lead-in to additional content. Thiscontent is a little bit longer."
